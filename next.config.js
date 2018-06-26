d9bc26539d18b7cad91c015a5a5cb5abdd20a709
niu_hl <niu_hl@suixingpay.com>
2018-06-26 16:40:52 +0800

    添加 webpack analyzerPort模块

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /* eslint-disable */
 const withLess = require('@zeit/next-less')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+// import withLess from '@zeit/next-less'
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-module.exports = {
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
   distDir: 'build',
+  // generateEtags: false,
   useFileSystemPublicRoutes: false,
-  ...withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-    },
-  })
-}
+  webpack: (config, { isServer }) => {
+    config.plugins.push(new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerPort: isServer ? 8888 : 8889,
+      openAnalyzer: true
+    }))
+    return config;
+  }
+})
